24a7ce1a67e00e710e390fcc796d25e1c244883a
Parv-gugnani <9765.parv@gmail.com>
2023-08-24 18:59:26 +0530

    function

---
+++ Function Composition.js
@@ -0,0 +1,12 @@
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+var compose = function (functions) {
+  return function (x) {
+    for (const fn of functions.reverse()) {
+      x = fn(x); //
+    }
+    return x;
+  };
+};
